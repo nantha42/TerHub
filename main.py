9c003fb682e755a51a767c878e667254dd9e2e74
nantha42 <krnantha13@gmail.com>
2020-08-10 12:28:59 +0530

    Added delete function

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import sys
 import getopt
 
 
-access_token = "1f003ae8e8b2210d72630e6f3150b921a522a868"
-user = "nantha42"
-
 def create_repository(name,private=False):
     
     response = requests.post(
@@ -24,20 +21,47 @@ def create_repository(name,private=False):
         file = open("out.json","w")
         json.dump(out,file)
         file.close()
+        print("Created Successfully")
         print("URL: ",out["clone_url"])
     else:
         print(response.status_code)
         print(response.text)
+
+def delete_repository(name,user):
+    response = requests.delete(
+        url =f"https://api.github.com/repos/{user}/{name}",
+        headers={
+            "Authorization":f"token {access_token}",
+            "Accept": "application/vnd.github.v3+json"
+        },
+        data = json.dumps({'name':name})
+    )
+    if 200< response.status_code <210:
+        print(response.status_code)
+        print(f"Deleted repository {name} successfully")
+    else:
+        print(response.status_code)
+        print(response.text)
+
+
     
 if __name__ == '__main__':
+    access_token = None
+    user = None 
+    with open("tokens.dat","r") as file:
+        access_token = file.read().split("\n")[-1]
+    with open("users.dat","r") as file:
+        user = file.read().split("\n")[-1]
+    
     arguments = len(sys.argv)
-    # opt,rem = getopt.getopt(['-aval'],'a:')
-    opt,rem = getopt.getopt(sys.argv[1:],"",["name =","private =","hel="])
+    
+    opt,rem = getopt.getopt(sys.argv[1:],"cd",["name =","private =","hel="])
     name = None
-    private = False
+    private = None
+    option = None
     for i,v in opt:
         if i=="--name ":
-            name = v 
+            name = v
         elif i == "--private ":
             if v == 'false':
                 private = False
@@ -45,6 +69,19 @@ if __name__ == '__main__':
                 private = True 
             else:
                 private = False
-    print("Name: ",name)
-    print("Private: ",private)
-    create_repository(name,private)
+        elif i=='-c':
+            option = "create"
+        elif i=='-d':
+            option = "delete"
+
+    # print("Name: ",name)
+    # print("Private: ",private)
+    # print(f"Option: {option}")
+    
+    if option == "create":
+        if name != None:
+            create_repository(name,private)
+
+    elif option == "delete":
+        if name!=None:
+            delete_repository(name,user)
